3e113fe51c673ef3c83c03f2ddcdda8cfc90b3c2
Bassel Sallam <Bassela.sallam@gmail.com>
2025-08-30 16:01:37 +0300

    envrdis

--- src/cache/cacheClient.ts
+++ src/cache/cacheClient.ts
@@ -1,8 +1,10 @@
 import type { RedisOptions } from "ioredis";
+import dotenv from "dotenv";
+dotenv.config();
 
 export function redisConnectionOptions(): RedisOptions {
   const redisUrl = new URL(
-    "redis://default:9A9JZHho3YECKguKxxWoirSaRrapXe3k@redis-10772.crce176.me-central-1-1.ec2.redns.redis-cloud.com:10772"
+    process.env.REDIS_URL || ""
   );
 
   return {
